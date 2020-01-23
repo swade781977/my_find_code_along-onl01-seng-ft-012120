7d1a373987de1970d690b69bd4e84f44e4a03558
Chris Wade <swade781977@yahoo.com>
2020-01-23 16:51:30 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,8 @@ require 'pry'
 def my_find(collection)
   count = 0
   while count < collection.length
-    yield(collection[count])
+    if yield(collection[count])
+      return collection[i]
     count+=1
   end
 end
